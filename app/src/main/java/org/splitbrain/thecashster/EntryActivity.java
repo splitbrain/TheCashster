9c7b79df5decd817cc0d8aa4185367616cda52d5
Andreas Gohr <andi@splitbrain.org>
2017-10-31 22:55:46 +0100

    first proper go at google sheets syncing

--- app/src/main/java/org/splitbrain/thecashster/EntryActivity.java
+++ app/src/main/java/org/splitbrain/thecashster/EntryActivity.java
@@ -1,8 +1,11 @@
 package org.splitbrain.thecashster;
 
 import android.Manifest;
+import android.accounts.AccountManager;
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.location.Location;
 import android.os.Bundle;
 import android.os.Vibrator;
@@ -19,11 +22,16 @@ import android.widget.Toast;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.util.ExponentialBackOff;
+import com.google.api.services.sheets.v4.SheetsScopes;
 
 import org.splitbrain.thecashster.model.Place;
 import org.splitbrain.thecashster.model.Transaction;
+import org.splitbrain.thecashster.sheets.SheetsTask;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Locale;
 
 import butterknife.BindView;
@@ -35,13 +43,31 @@ import pub.devrel.easypermissions.EasyPermissions;
 public class EntryActivity extends AppCompatActivity implements
         GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
-    static final int REQUEST_FINE_LOCATION = 1001;
+    private final String TAG = this.getClass().getSimpleName();
+
+    // preference key to store which account to use for the spreadsheet API
+    private static final String PREF_ACCOUNT_NAME = "accountName";
+    // requested permissions for the spreadsheet API
+    private final String[] SCOPES = {SheetsScopes.SPREADSHEETS};
+
+    // identifiers for request callbacks
+    public static final int REQUEST_ACCOUNT_PICKER = 1000;
+    public static final int REQUEST_AUTHORIZATION = 1001;
+    public static final int REQUEST_GOOGLE_PLAY_SERVICES = 1002;
+    public static final int REQUEST_PERMISSION_GET_ACCOUNTS = 1003;
+    public static final int REQUEST_FINE_LOCATION = 1004;
+
+    // business logic
     private PlacesAdapter mAdapter;
     private String mAmount = "";
     private int mNeg = -1;
-    private GoogleApiClient mGoogleApiClient;
     private Location mLastLocation;
 
+    // google API clients
+    private GoogleApiClient mGoogleApiClient;
+    GoogleAccountCredential mCredential;
+
+    // views
     @BindView(R.id.textSearch)
     TextView vTextSearch;
     @BindView(R.id.buttonSearch)
@@ -58,7 +84,7 @@ public class EntryActivity extends AppCompatActivity implements
         ButterKnife.bind(this);
 
         // attach adapter to our list view
-        final ListView listview = (ListView) findViewById(R.id.listPlaces);
+        final ListView listview = findViewById(R.id.listPlaces);
         mAdapter = new PlacesAdapter(this, new ArrayList<Place>());
         listview.setAdapter(mAdapter);
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -83,6 +109,11 @@ public class EntryActivity extends AppCompatActivity implements
                     .addApi(LocationServices.API)
                     .build();
         }
+
+        // Initialize Google Credential object
+        mCredential = GoogleAccountCredential.usingOAuth2(
+                this, Arrays.asList(SCOPES)
+        ).setBackOff(new ExponentialBackOff());
     }
 
     /**
@@ -115,8 +146,13 @@ public class EntryActivity extends AppCompatActivity implements
                 storeTransaction();
                 return; // we're done
             default:
+                String key = b.getText().toString();
+                if ((key.equals("0") || key.equals("00")) && getAmount() == 0.0) {
+                    return; // don't add zeros to a zero
+                }
+
                 if (mAmount.length() < 8) {
-                    mAmount = mAmount.concat(b.getText().toString());
+                    mAmount = mAmount.concat(key);
                 } else {
                     Toast.makeText(getApplicationContext(), "Sorry, that's just too much",
                             Toast.LENGTH_SHORT).show();
@@ -161,10 +197,107 @@ public class EntryActivity extends AppCompatActivity implements
         realm.commitTransaction();
 
         // FIXME start new task for transfering the data to Google Sheets
+        startSheetsSync();
 
+        // reset the interface
+        mAdapter.selectItem(-1);
         mAmount = "";
         updateAmountView();
     }
+
+
+    private void startSheetsSync() {
+        // fixme check for network connectivity first
+
+        if (mCredential.getSelectedAccountName() == null) {
+            chooseAccount();
+            return;
+        }
+
+        SheetsTask task = new SheetsTask(this, mCredential);
+        task.execute();
+    }
+
+    /**
+     * Attempts to set the account used with the API credentials. If an account
+     * name was previously saved it will use that one; otherwise an account
+     * picker dialog will be shown to the user. Note that the setting the
+     * account to use with the credentials object requires the app to have the
+     * GET_ACCOUNTS permission, which is requested here if it is not already
+     * present. The AfterPermissionGranted annotation indicates that this
+     * function will be rerun automatically whenever the GET_ACCOUNTS permission
+     * is granted.
+     */
+    @AfterPermissionGranted(REQUEST_PERMISSION_GET_ACCOUNTS)
+    private void chooseAccount() {
+        if (EasyPermissions.hasPermissions(this, Manifest.permission.GET_ACCOUNTS)) {
+            // get account name from preferences
+            String accountName = getPreferences(Context.MODE_PRIVATE)
+                    .getString(PREF_ACCOUNT_NAME, null);
+            if (accountName != null) {
+                // account has been set, retry the sync task
+                mCredential.setSelectedAccountName(accountName);
+                startSheetsSync();
+            } else {
+                // Start a dialog from which the user can choose an account
+                startActivityForResult(mCredential.newChooseAccountIntent(), REQUEST_ACCOUNT_PICKER);
+            }
+        } else {
+            // Request the GET_ACCOUNTS permission via a user dialog
+            EasyPermissions.requestPermissions(
+                    this,
+                    "The contacts permission is needed to authenticate your spreadsheet account",
+                    REQUEST_PERMISSION_GET_ACCOUNTS,
+                    Manifest.permission.GET_ACCOUNTS
+            );
+            // when permission was granted this method is called again, thanks to the annotation
+        }
+    }
+
+    /**
+     * Called when an activity launched here (specifically, AccountPicker
+     * and authorization) exits, giving you the requestCode you started it with,
+     * the resultCode it returned, and any additional data from it.
+     *
+     * @param requestCode code indicating which activity result is incoming.
+     * @param resultCode  code indicating the result of the incoming
+     *                    activity result.
+     * @param data        Intent (containing result data) returned by incoming
+     *                    activity result.
+     */
+    @Override
+    protected void onActivityResult(
+            int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_ACCOUNT_PICKER:
+                if (resultCode == RESULT_OK && data != null &&
+                        data.getExtras() != null) {
+                    // save the account name to the preferences
+                    String accountName =
+                            data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+                    if (accountName != null) {
+                        SharedPreferences settings =
+                                getPreferences(Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = settings.edit();
+                        editor.putString(PREF_ACCOUNT_NAME, accountName);
+                        editor.apply();
+                        mCredential.setSelectedAccountName(accountName);
+
+                        // try the sync task again
+                        startSheetsSync();
+                    }
+                }
+                break;
+            case REQUEST_AUTHORIZATION:
+                if (resultCode == RESULT_OK) {
+                    // try the sync task again
+                    startSheetsSync();
+                }
+                break;
+        }
+    }
+
 
     /**
      * Get the current amount as float
--- app/src/main/java/org/splitbrain/thecashster/sheets/SheetsTask.java
+++ app/src/main/java/org/splitbrain/thecashster/sheets/SheetsTask.java
@@ -1,37 +1,226 @@
 package org.splitbrain.thecashster.sheets;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.util.Log;
 
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.AppendValuesResponse;
+import com.google.api.services.sheets.v4.model.BatchUpdateSpreadsheetRequest;
+import com.google.api.services.sheets.v4.model.Request;
+import com.google.api.services.sheets.v4.model.Spreadsheet;
+import com.google.api.services.sheets.v4.model.SpreadsheetProperties;
+import com.google.api.services.sheets.v4.model.UpdateSpreadsheetPropertiesRequest;
+import com.google.api.services.sheets.v4.model.ValueRange;
+
+import org.splitbrain.thecashster.EntryActivity;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Andreas Gohr andi@splitbrain.org
  */
 
-abstract class SheetsTask extends AsyncTask<Void, Void, SheetsTask> {
+public class SheetsTask extends AsyncTask<Void, Void, SheetsTask> {
+    // for logging
+    private final String TAG = this.getClass().getSimpleName();
+    // the preference that holds our google sheets document ID
+    private static final String PREF_SHEET_ID = "sheetID";
+    private static final String SHEET_TITLE = "TheCashster";
 
     private OnTaskCompleted mListener;
-
     private com.google.api.services.sheets.v4.Sheets mService = null;
     private Exception mLastError = null;
+    private EntryActivity mActivity;
 
     /**
      * Constructor
-     * 
-     * @param credential
      */
-    SheetsTask(GoogleAccountCredential credential) {
+    public SheetsTask(EntryActivity act, GoogleAccountCredential credential) {
+        mActivity = act;
         HttpTransport transport = AndroidHttp.newCompatibleTransport();
         JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+
         mService = new com.google.api.services.sheets.v4.Sheets.Builder(
                 transport, jsonFactory, credential)
                 .setApplicationName("TheCashster")
                 .build();
     }
+
+    @Override
+    protected SheetsTask doInBackground(Void... voids) {
+//        Realm realm = Realm.getDefaultInstance();
+//        RealmResults<Transaction> transactions = realm.where(Transaction.class).findAll();
+//        for (Transaction tx : transactions) {
+//            Log.i(TAG, tx.getDt().toString());
+//        }
+
+        Log.i(TAG, "Sync task started");
+
+        try {
+            //testing();
+            String docId = getOrCreateDocument();
+            Log.i(TAG, "DocID aquired: ".concat(docId));
+
+            // fixme get all pending transactions, append them and then delete them locally
+
+        } catch (IOException e) {
+            mLastError = e;
+            cancel(true);
+        }
+
+        return this;
+    }
+
+    /**
+     * Generates the headers needed for the spreadsheet
+     *
+     * @return the headers
+     */
+    private List<List<Object>> getHeaders() {
+        List<List<Object>> values = new ArrayList<>();
+        List<Object> row = new ArrayList<>();
+
+        row.add("TX ID");
+        row.add("Amount");
+        row.add("Date");
+        row.add("Place");
+        row.add("ForeignID");
+        row.add("Latitude");
+        row.add("Longitude");
+        values.add(row);
+
+        return values;
+    }
+
+    /**
+     * Append data to the spreadsheet
+     *
+     * @param docId the document ID
+     * @param values two-dimensional array of cells to append
+     * @throws IOException
+     */
+    private void append(String docId, List<List<Object>> values) throws IOException {
+        String range = "A1:B1";
+
+        ValueRange requestBody = new ValueRange();
+        requestBody.setValues(values);
+
+        Sheets.Spreadsheets.Values.Append request =
+                mService.spreadsheets().values().append(docId, range, requestBody).setValueInputOption("USER_ENTERED");
+
+        AppendValuesResponse response = request.execute();
+        System.out.println(response);
+    }
+
+    /**
+     * Get the ID of the spreadsheet to sync to
+     * <p>
+     * On first time use this will create a new Document, set the title and add headers.
+     * Subsequent uses will pull the document ID from the preferences and just check that the
+     * doc hasn't been deleted
+     *
+     * @return the documentID of our spreadsheet
+     * @throws IOException when something goes wrong
+     */
+    private String getOrCreateDocument() throws IOException {
+        // get doc from preferences and check it still exists
+        String docID = mActivity.getPreferences(Context.MODE_PRIVATE)
+                .getString(PREF_SHEET_ID, null);
+        if (docID != null) {
+            try {
+                mService.spreadsheets().get(docID).execute();
+                return docID;
+            } catch (GoogleJsonResponseException e) {
+                if (e.getStatusCode() == 404) {
+                    Log.e(TAG, "Known doc is not accessible, we forget about it");
+                } else {
+                    throw e;
+                }
+            }
+        }
+
+        // create new document
+        Spreadsheet doc = mService.spreadsheets().create(new Spreadsheet()).execute();
+
+        // add title and locale
+        List<Request> requests = new ArrayList<>();
+        requests.add(
+                new Request()
+                        .setUpdateSpreadsheetProperties(
+                                new UpdateSpreadsheetPropertiesRequest()
+                                        .setProperties(
+                                                new SpreadsheetProperties()
+                                                        .setTitle(SHEET_TITLE)
+                                        )
+                                        .setFields("title")
+                        )
+        );
+
+        requests.add(
+                new Request()
+                        .setUpdateSpreadsheetProperties(
+                                new UpdateSpreadsheetPropertiesRequest()
+                                        .setProperties(
+                                                new SpreadsheetProperties()
+                                                        .setLocale("en_US")
+                                        )
+                                        .setFields("locale")
+                        )
+        );
+        BatchUpdateSpreadsheetRequest body =
+                new BatchUpdateSpreadsheetRequest().setRequests(requests);
+        mService.spreadsheets().batchUpdate(doc.getSpreadsheetId(), body).execute();
+
+        // add header line
+        append(doc.getSpreadsheetId(), getHeaders());
+
+        // remember doc ID in preferences
+        SharedPreferences settings =
+                mActivity.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(PREF_SHEET_ID, doc.getSpreadsheetId());
+        editor.apply();
+
+        Log.i(TAG, doc.getSpreadsheetId());
+        return doc.getSpreadsheetId();
+    }
+
+
+    /**
+     * Handle the cancellation of the task
+     * <p>
+     * This happens when any exception is thrown. Ususally we can't do anything in that
+     * case and just silently ignore the error and hope it will work next time.
+     * However we do handle missing authorization here by opening a dialog about it and
+     * having the result passed back to our EntryActivity. Once proper auth is available,
+     * the task will be restarted again.
+     */
+    @Override
+    protected void onCancelled() {
+        if (mLastError != null) {
+            if (mLastError instanceof UserRecoverableAuthIOException) {
+                mActivity.startActivityForResult(
+                        ((UserRecoverableAuthIOException) mLastError).getIntent(),
+                        EntryActivity.REQUEST_AUTHORIZATION);
+            }
+        }
+
+
+        Log.e(TAG, "task was cancelled", mLastError);
+    }
+
+    // region OnTaskCompleted handling
 
     /**
      * Interface for TaskCompleted mListener
@@ -58,4 +247,5 @@ abstract class SheetsTask extends AsyncTask<Void, Void, SheetsTask> {
         }
     }
 
+    // endregion
 }
